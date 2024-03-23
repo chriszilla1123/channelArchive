02f5fcdacb5e42e70f63e9b1ebe4bd1b08bee5f0
= <=>
2024-03-23 14:04:25 -0400

    restructure functions for readability

--- channelArchive.py
+++ channelArchive.py
@@ -21,28 +21,55 @@ SITE_RUMBLE = "rumble"
 LOGGING_VERBOSE = "verbose"
 LOGGING_DELETED = "deleted"
 
-def downloadVideos(channel):
-#Downloads all the videos from a channel using youtube-dl
-#Relys on channel.videos being set, from the loadVideos function
-    if len(channel.videos) <= 0:
-        log("\nNo videos found for " + str(channel))
-        return
+def main():
+    loadCommandLineArgs(sys.argv)
+    timeStart = time.time()
+    startMessage = "\n" +  str(datetime.datetime.now()) + "\n"
+    startMessage += "channelArchive.py - A youtube channel downloader"
+    log(startMessage, "high")
+    loadChannels()
 
-    ytdl_log = open(install_dir + 'ytdl.log', 'a+')
-    for i, video in enumerate(channel.videos, start=1):
-        log("Downloading video (" + str(i) + " / " + str(len(channel.videos)) + "): " + str(video), "high")
-        url = "https://www.youtube.com/watch?v=" + video.videoID
-        outputFile = base_dir + channel.channelDir + '%(upload_date)s - %(title)s - %(id)s.%(ext)s'
-        ytdl_log = open(install_dir + 'ytdl.log', 'a+')
-        myCall = [youtube_dl_location, '-o', outputFile, '-f', 'best', '--cookies', install_dir + 'cookies.txt',  '--',  url]
-        if "--best" in channel.args:
-            myCall = [youtube_dl_location, '-o', outputFile, '-f', 
-                      '((571/272/402/337/315/313/401/336/308/400/271/335/303/299/399/137/248/334/302/298/398/247/136/333/244/135/397/332/243/134/396/331/242/133/395/330/160/394/278)[protocol!=http_dash_segments])+(bestaudio[acodec=opus]/bestaudio[protocol!=http_dash_segments])/best',
-                        '--', url]
+    threads = []
+    for channel in channels:
+        loadVideos(channel)
+        downloadVideos(channel)
 
-        call(myCall, stdout=ytdl_log, stderr=ytdl_log)
-    ytdl_log.close()
+    timeElapsed = round(time.time() - timeStart, 2)
+    log("Finished in " + str(timeElapsed) + " seconds.", "high")
+    
 
+def loadChannels():
+###Loads the configuration and channel options from the 'channels' file.
+    global base_dir
+    with open(install_dir + 'channels') as file:
+        for line in file:
+            line = stripWhitespace(line)
+            if len(line) == 0:
+                continue
+            if line[0] == "#":
+                continue
+            if "base_dir" in line:
+                base_dir = line.split("=")[-1]
+                base_dir = stripWhitespace(base_dir)
+                if base_dir[-1] != "/":
+                    base_dir += "/"
+                log("Setting base directory to " + base_dir)
+            args = ""
+            if line[0] == "[":
+                channelName = line.split("[")[1].split("]")[0]
+                channelID = line.split("[")[2].split("]")[0]
+                channelDir = line.split("[")[3].split("]")[0]
+                if len(line.split("[")) >= 5:
+                        args = line.split("[")[4].split("]")[0]
+
+                if channelDir[-1] != "/":
+                    channelDir += "/"
+                channel = Channel(channelName, channelID, channelDir, args)
+                channels.append(channel)
+                #Create Dir if it doesn't exist
+                dirToCheck = base_dir + channelDir
+                if not os.path.exists(dirToCheck):
+                    os.makedirs(dirToCheck)
 
 def loadVideos(channel):
 # Gets all the Video IDs from a channel
@@ -86,43 +113,27 @@ def loadVideos(channel):
     except:
         log("Error downloading videos by " + str(channel))
 
+def downloadVideos(channel):
+#Downloads all the videos from a channel using youtube-dl
+#Relys on channel.videos being set, from the loadVideos function
+    if len(channel.videos) <= 0:
+        log("\nNo videos found for " + str(channel))
+        return
 
-def stripWhitespace(string):
-    return string.strip()
-	
-    
-def loadChannels():
-###Loads the configuration and channel options from the 'channels' file.
-    global base_dir
-    with open(install_dir + 'channels') as file:
-        for line in file:
-            line = stripWhitespace(line)
-            if len(line) == 0:
-                continue
-            if line[0] == "#":
-                continue
-            if "base_dir" in line:
-                base_dir = line.split("=")[-1]
-                base_dir = stripWhitespace(base_dir)
-                if base_dir[-1] != "/":
-                    base_dir += "/"
-                log("Setting base directory to " + base_dir)
-            args = ""
-            if line[0] == "[":
-                channelName = line.split("[")[1].split("]")[0]
-                channelID = line.split("[")[2].split("]")[0]
-                channelDir = line.split("[")[3].split("]")[0]
-                if len(line.split("[")) >= 5:
-                        args = line.split("[")[4].split("]")[0]
+    ytdl_log = open(install_dir + 'ytdl.log', 'a+')
+    for i, video in enumerate(channel.videos, start=1):
+        log("Downloading video (" + str(i) + " / " + str(len(channel.videos)) + "): " + str(video), "high")
+        url = "https://www.youtube.com/watch?v=" + video.videoID
+        outputFile = base_dir + channel.channelDir + '%(upload_date)s - %(title)s - %(id)s.%(ext)s'
+        ytdl_log = open(install_dir + 'ytdl.log', 'a+')
+        myCall = [youtube_dl_location, '-o', outputFile, '-f', 'best', '--cookies', install_dir + 'cookies.txt',  '--',  url]
+        if "--best" in channel.args:
+            myCall = [youtube_dl_location, '-o', outputFile, '-f', 
+                      '((571/272/402/337/315/313/401/336/308/400/271/335/303/299/399/137/248/334/302/298/398/247/136/333/244/135/397/332/243/134/396/331/242/133/395/330/160/394/278)[protocol!=http_dash_segments])+(bestaudio[acodec=opus]/bestaudio[protocol!=http_dash_segments])/best',
+                        '--', url]
 
-                if channelDir[-1] != "/":
-                    channelDir += "/"
-                channel = Channel(channelName, channelID, channelDir, args)
-                channels.append(channel)
-                #Create Dir if it doesn't exist
-                dirToCheck = base_dir + channelDir
-                if not os.path.exists(dirToCheck):
-                    os.makedirs(dirToCheck)
+        call(myCall, stdout=ytdl_log, stderr=ytdl_log)
+    ytdl_log.close()
 
 def loadCommandLineArgs(args):
     global logging_options
@@ -132,7 +143,6 @@ def loadCommandLineArgs(args):
             logging_options.append(LOGGING_VERBOSE)
         if arg == "--deleted" or arg == "-d":
             logging_options.append(LOGGING_DELETED)
-
 
 def log(message, priority="low"):
     global install_dir
@@ -144,22 +154,8 @@ def log(message, priority="low"):
     elif priority == LOGGING_DELETED and LOGGING_DELETED in logging_options:
         print(message)
 
-
-def main():
-    loadCommandLineArgs(sys.argv)
-    timeStart = time.time()
-    startMessage = "\n" +  str(datetime.datetime.now()) + "\n"
-    startMessage += "channelArchive.py - A youtube channel downloader"
-    log(startMessage, "high")
-    loadChannels()
-
-    threads = []
-    for channel in channels:
-        loadVideos(channel)
-        downloadVideos(channel)
-
-    timeElapsed = round(time.time() - timeStart, 2)
-    log("Finished in " + str(timeElapsed) + " seconds.", "high")
+def stripWhitespace(string):
+    return string.strip()
 
 class Sites(Enum):
     YOUTUBE = 1
